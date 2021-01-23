11f89fbd8787c8b9ca570da444b9508cb8137622
Neil Rush <neileorushio@gmail.com>
2021-01-22 22:08:42 -0600

    Created Categories for commands

---
+++ PingBotCS/Modules/Fun.cs
@@ -0,0 +1,38 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PingBotCS.Modules
+{
+    class Fun : ModuleBase
+    {
+        [Command("roulette")]
+        [RequireUserPermission(GuildPermission.MoveMembers)]
+        public async Task Roulette()
+        {
+            await Context.Guild.DownloadUsersAsync();
+            var message = await Context.Channel.SendMessageAsync("Kicking random user (who will it be :O)...");
+            await Task.Delay(1000);
+
+            await message.DeleteAsync();
+            var voiceChannels = await Context.Guild.GetVoiceChannelsAsync();
+
+            var userList = voiceChannels
+                .Select(async channel => await channel.GetUsersAsync().FlattenAsync())
+                .Select(t => t.Result)
+                .Aggregate(new List<IGuildUser>(), (acc, rhs) => acc.Concat(rhs).ToList());
+
+            Random randomNumber = new Random();
+            IGuildUser unluckyUser = userList[randomNumber.Next(userList.Count)];
+            await unluckyUser.ModifyAsync(x => x.Channel = null);
+            message = await Context.Channel.SendMessageAsync($"{unluckyUser.Nickname} has been chosen...");
+            await Task.Delay(1000);
+            await message.DeleteAsync();
+        }
+    }
+}
--- PingBotCS/Modules/General.cs
+++ PingBotCS/Modules/General.cs
@@ -12,20 +12,6 @@ namespace PingBotCS.Modules
 {
     public class General : ModuleBase
     {
-        private Embed CreateInfoEmbed(ulong id, DateTimeOffset createdAt, DateTimeOffset joinedAt, string avatarUrl, IEnumerable<SocketRole> roles)
-        {
-            return new EmbedBuilder()
-                .WithThumbnailUrl(avatarUrl)
-                .WithDescription("In this message you can see some information about yourself!")
-                .WithColor(new Color(33, 176, 252))
-                .AddField("User ID", id, true)
-                .AddField("Created at", createdAt.ToString("MM/dd/yyyy"), true)
-                .AddField("Joined at", joinedAt.ToString("MM/dd/yyyy"), true)
-                .AddField("Roles", string.Join(" ", roles.Select(x => x.Mention)))
-                .WithCurrentTimestamp()
-                .Build();
-        }
-
         [Command("ping")]
         public async Task Ping()
         {
@@ -42,21 +28,20 @@ namespace PingBotCS.Modules
                 user = Context.User as SocketGuildUser;
             }
 
-            embed = CreateInfoEmbed(user.Id, user.CreatedAt, user.JoinedAt.Value, user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl(), user.Roles);
+            embed = new EmbedBuilder()
+                .WithThumbnailUrl(user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl())
+                .WithDescription("In this message you can see some information about yourself!")
+                .WithColor(new Color(33, 176, 252))
+                .AddField("User ID", user.Id, true)
+                .AddField("Created at", user.CreatedAt.ToString("MM/dd/yyyy"), true)
+                .AddField("Joined at", user.JoinedAt.Value.ToString("MM/dd/yyyy"), true)
+                .AddField("Roles", string.Join(" ", user.Roles.Select(x => x.Mention)))
+                .WithCurrentTimestamp()
+                .Build();
             await Context.Channel.SendMessageAsync(null, false, embed);
         }
 
-        [Command("purge")]
-        [RequireUserPermission(GuildPermission.ManageMessages)]
-        public async Task Purge(int amount)
-        {
-            var messages = await Context.Channel.GetMessagesAsync(amount + 1).FlattenAsync();
-            await (Context.Channel as SocketTextChannel).DeleteMessagesAsync(messages);
 
-            var message = await Context.Channel.SendMessageAsync($"{messages.Count()} messages deleted successfully!");
-            await Task.Delay(2500);
-            await message.DeleteAsync();
-        }
 
         [Command("server")]
         public async Task Server()
@@ -71,29 +56,6 @@ namespace PingBotCS.Modules
                 .AddField("Online users", (Context.Guild as SocketGuild).Users.Where(x => x.Status != UserStatus.Offline).Count() + " members", true);
             var embed = builder.Build();
             await Context.Channel.SendMessageAsync(null, false, embed);
-        }
-
-        [Command("roulette")]
-        [RequireUserPermission(GuildPermission.MoveMembers)]
-        public async Task Roulette() {
-            await Context.Guild.DownloadUsersAsync();
-            var message = await Context.Channel.SendMessageAsync("Kicking random user (who will it be :O)...");
-            await Task.Delay(1000);
-
-            await message.DeleteAsync();
-            var voiceChannels = await Context.Guild.GetVoiceChannelsAsync();
-
-            var userList = voiceChannels
-                .Select(async channel => await channel.GetUsersAsync().FlattenAsync())
-                .Select(t => t.Result)
-                .Aggregate(new List<IGuildUser>(), (acc, rhs) => acc.Concat(rhs).ToList());
-
-            Random randomNumber = new Random();
-            IGuildUser unluckyUser = userList[randomNumber.Next(userList.Count)];
-            await unluckyUser.ModifyAsync(x => x.Channel = null);
-            message = await Context.Channel.SendMessageAsync($"{unluckyUser.Nickname} has been chosen...");
-            await Task.Delay(1000);
-            await message.DeleteAsync();
         }
     }
 }
