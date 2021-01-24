46c0f699b259198e893f7f383c74f2e3c28f9a75
Neil Rush <neileorushio@gmail.com>
2021-01-24 02:42:27 -0600

    Updated to new codebase

--- PingBotCS/Services/CommandHandler.cs
+++ PingBotCS/Services/CommandHandler.cs
@@ -1,57 +1,52 @@
+﻿using System;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Addons.Hosting;
 using Discord.Commands;
 using Discord.WebSocket;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PingBotCS.Services
+namespace Template.Services
 {
-    public class CommandHandler
+    public class CommandHandler : InitializedService
     {
-        public static IServiceProvider _provider;
-        public static DiscordSocketClient _discord;
-        public static CommandService _commands;
-        public static IConfigurationRoot _config;
+        private readonly IServiceProvider _provider;
+        private readonly DiscordSocketClient _client;
+        private readonly CommandService _service;
+        private readonly IConfiguration _config;
 
-        public CommandHandler(DiscordSocketClient discord, CommandService commands, IConfigurationRoot config, IServiceProvider provider)
+        public CommandHandler(IServiceProvider provider, DiscordSocketClient client, CommandService service, IConfiguration config)
         {
             _provider = provider;
+            _client = client;
+            _service = service;
             _config = config;
-            _discord = discord;
-            _commands = commands;
+        }
 
-            _discord.Ready += OnReady;
-            _discord.MessageReceived += OnMessageReceived;
+        public override async Task InitializeAsync(CancellationToken cancellationToken)
+        {
+            _client.MessageReceived += OnMessageReceived;
+            _service.CommandExecuted += OnCommandExecuted;
+            await _service.AddModulesAsync(Assembly.GetEntryAssembly(), _provider);
         }
 
         private async Task OnMessageReceived(SocketMessage arg)
         {
-            var msg = arg as SocketUserMessage;
+            if (!(arg is SocketUserMessage message)) return;
+            if (message.Source != MessageSource.User) return;
 
-            if (msg.Author.IsBot) return;
-            var context = new SocketCommandContext(_discord, msg);
+            var argPos = 0;
+            if (!message.HasStringPrefix(_config["prefix"], ref argPos) && !message.HasMentionPrefix(_client.CurrentUser, ref argPos)) return;
 
-            int pos = 0;
-            if(msg.HasStringPrefix(_config["prefix"], ref pos) || msg.HasMentionPrefix(_discord.CurrentUser, ref pos))
-            {
-                var result = await _commands.ExecuteAsync(context, pos, _provider);
-
-                if(!result.IsSuccess)
-                {
-                    var reason = result.Error;
-
-                    await context.Channel.SendMessageAsync($"The following error occured: \n {reason}");
-                    Console.WriteLine(reason);
-                }
-            }
+            var context = new SocketCommandContext(_client, message);
+            await _service.ExecuteAsync(context, argPos, _provider);
         }
 
-        private Task OnReady()
+        private async Task OnCommandExecuted(Optional<CommandInfo> command, ICommandContext context, IResult result)
         {
-            Console.WriteLine($"Connected as {_discord.CurrentUser.Username}#{_discord.CurrentUser.Discriminator}");
-            return Task.CompletedTask;
+            if (command.IsSpecified && !result.IsSuccess) await context.Channel.SendMessageAsync($"Error: {result}");
         }
     }
 }
